7c3ebcdc8dfa0096d9ea17acb3dabdca7528a14a
wisteria-tiger <dartingsparrow77@icloud.com>
2026-04-20 00:18:13 -0500

    Began working on code for what is currently written of story outline

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #-----------------DOOMED DUNGEON-----------------
-#Summary:
 #Set in a relatively generic fantasy world, with knights, wizards, dwarves, skeletons, slimes, dragons - and dungeons, of course.
 #You play as a knight who ventures off to fight through a legendary dungeon with promise of great treasures. 
 #However, you find that the dungeon is, for lack of a better term, incredibly lame. 
@@ -12,13 +11,32 @@
 #------------------------------------------------
 
 #-----------------------
-#PLAYER SERTUP AND WORLD INTRODUCTION
+#PLAYER SETUP AND WORLD INTRODUCTION
 #-----------------------
+
 #Prompt for name
 
+name = input("Salutations brave adventurer! What do you call yourself in this fantastical world of ours?" )
+weapon = ""
 #Prompt for weapon of choice (sword, axe, or spear)
-
+weapon_choice = input("So your name is Knight %s, huh? Shall you wield the valiant sword(1), the imposing axe(2), or the tactical spear?(3)" %name)
+def choose_weapon(weapon_choice):
+    match weapon_choice:
+        case 1:
+            print("You have chosen the valiant sword!")
+            weapon = "sword"
+        case 2:
+            weapon = "axe"
+            return "You have chosen the imposing axe!", weapon
+        case 3:
+            print()
+            weapon = "spear"
+            return "You have chosen the tactical spear!", weapon
+        case _:
+            weapon = "fists"
+            return "You have chosen to use your bare fists! Since you have such an attitude!", weapon
 #Give the lowdown on what the setting is like
+player = {name, weapon}
 
 #Have the player arrive at the dungeon's entrance after gathering supplies, accentuating how excited the knight is to explore the famed dungeon
 #CHOICE 1 - Give them the choice to enter the dungeon, or decide just to straight up leave because the entrance looks really depressing compared to the poster advertising the dungeon
@@ -39,14 +57,15 @@
 #The player continues on, taking note of the encounter.
 
 #----------------------
-#PLAYER LEARNS OF THE DUGEON'S PLIGHT
+#PLAYER LEARNS OF THE DUNGEON'S PLIGHT
 #----------------------
 
-#Player finds goblin who looks like hes dying of starvation. He makes a weak swing that pings off the player's armor hharmlessly. The goblin thenn breaks down and starts crying. Out of pity, the player can: 
+#Player finds goblin who looks like hes dying of starvation. He makes a weak swing that pings off the player's armor harmlessly. The goblin thenn breaks down and starts crying. Out of pity, the player can: 
 #1. PUt it out of its misery with a quick strike.
 #2. Ask why it is in its current state. 
 
 # If the player kills the goblin, they are none the wiser to the dungeon's plight and they decide to leave out of a newfound sadness. the game ends on a pretty depressing note.
-# If the player asks the goblin why its cryinng, the goblin decides to vent its frustrations by explaining that the dungeon has bassicaly gonne bankutpt with a severe lack of fighting spirit to go around,
-# so the player can then decide to somehow help the dungeon's inhabitantss or let the dungeon die. 
+# If the player asks the goblin why its cryinng, the goblin decides to vent its frustrations by explaining that the dungeon has basically gone bankutpt with a severe lack of fighting spirit to go around,
+# so the player can then decide to somehow help the dungeon's inhabitants or let the dungeon die. 
 
+# 
